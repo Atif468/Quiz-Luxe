c17d028c811b1cd19db57143da8192747f25159e
Mohd Atif Ansari <114794817+Atif468@users.noreply.github.com>
2024-03-03 15:47:55 +0530

    Add files via upload
    add api

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from "react";
+
 function Container() {
 
-    //api content/...........................
+    const [, setquestion] = useState();
 
-
-
-
-
-    //......................................
+    function run() {
+        fetch('https://opentdb.com/api.php?amount=20&category=18&difficulty=medium&type=multiple')
+            .then((resp) => {
+                if (!resp.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return resp.json();
+            })
+            .then((response) => {
+                return response.results;
+            })
+            .then((ques)=>
+            {
+                console.log(ques[0].question)
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    }
+    run();
     return (
-        <div className=" h-96 flex justify-center items-center gap-11 text-white">
+        <div className=" h-96 flex justify-center items-center gap-11 text-white ">
             <div className="flex flex-col gap-6 rounded h-auto w-96">
                 <h1 className="text-2xl">
                     Q. This is my first question.
